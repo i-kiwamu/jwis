e73fd41211b5512c049c800d22df714f92e1ce14
Kiwamu Ishikura <ishikura.kiwamu@gmail.com>
2016-04-27 17:56:12 +0900

    bug fix in setup.py
    bug fix in setup.py

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ setup(
         "Programming Language :: Python",
         "Programming Language :: Python :: 3.3",
         "Programming Language :: Python :: 3.4",
-        "Programming Language :: Python :: 3.5"
+        "Programming Language :: Python :: 3.5",
         "Development Status :: 4 - Beta",
         "Environment :: Console",
         "Intended Audience :: End Users/Desktop",
